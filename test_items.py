3070d9fd02ae84b7e0f215500e96c237e3e9b200
bio74 <bio74@list.ru>
2022-06-26 20:17:20 +0500

    'Lesson_3.6'

---
+++ test_items.py
@@ -0,0 +1,7 @@
+import time
+from selenium.webdriver.common.by import By
+def test_guest_should_see_login_link(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    time.sleep(30)
+    x = len(browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket"))
+    assert x > 0, 'Not found'
